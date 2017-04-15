dccf22f6486f92833ec945658fee4d4411e13e00
ashrafishaheen <ashrafi.shaikh96@gmail.com>
2017-04-15 20:01:13 +0530

    commit first page

--- js/my_script.js
+++ js/my_script.js
@@ -1,13 +1,40 @@
-$.ajax("http://acadprojects.com/py/fabsos/location?city_id=1", {
-	success: function(data) {
-		console.log("success")
-		a = data.data;
-		for(i = 1; i < a.length; i++) {
-			$("#loc").after("<li id='loc'><a href=''#'>"+a[i]["location_name"]+"</a></li>");
+$(document).ready(function(){
 
-		}
-	},
-	error: function() {
-		console.log("error");
-	}
+$.ajax("http://acadprojects.com/py/fabsos/location?city_id=1", {
+      success: function(content) {
+        console.log("success")
+        a = content.data;
+        for (i = 0; i < a.length; i++) {
+          $("#sel1").append("<option class='option_content' id="+i +">"+a[i]["location_name"]+"</option>");
+        //  $("#1").after("<li id='1'><a href='#'>"+a[i]["location_name"]+"</a></li>");
+
+
+        }
+      },
+      error: function() {
+         console.log("error");
+      }
+   });
+
+
+
+   });
+//
+last_value=22;
+
+$("#btn_order").click(function(){
+
+
+
+    loc = $(".form-control option:selected").val();
+
+
+
+
+    if (loc!=="Select location") {
+    console.log(loc);
+    localStorage.setItem("location", loc);
+    window.location.href = "https://ashigh-techinstitute.netlify.com/";
+  }
+
 });
